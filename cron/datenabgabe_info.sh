9bce7088bd9bb409b7bbdfc768099b2a9ba65708
edigonzales <edi.gonzales@gmail.com>
2013-06-02 16:48:15 +0000

    cronjob resp. bash script hinzugefügt

---
+++ cron/datenabgabe_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm /tmp/datenabgabe_info.json
+
+ogr2ogr  -f GeoJSON /tmp/datenabgabe_info.json PG:"dbname='rosebud' host='localhost' port='5432' user='stefan' password='ziegler12' schemas=av_nfgeometer tables=av_nfgeometer.datenabgabe_info_v"
+
+cp /tmp/datenabgabe_info.json /home/sogeo/sogeo.ch/docroot/extjs/mvc3/datenabgabe_info.json
+cp /tmp/datenabgabe_info.json /home/sogeo/sogeo.ch/docroot/daten/av/data/datenabgabe_info.json 
+
+chmod +r /home/sogeo/sogeo.ch/docroot/extjs/mvc3/datenabgabe_info.json
+chmod +r /home/sogeo/sogeo.ch/docroot/daten/av/data/datenabgabe_info.json
+
+
+
